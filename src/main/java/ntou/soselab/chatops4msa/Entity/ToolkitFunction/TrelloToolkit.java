4a1b079e9ad115a5c0ac0871fd4777ab4a0e979d
Robert9547 <00857002@email.ntou.edu.tw>
2024-04-11 00:46:15 +0800

    fix:trello toolkit
    改變呼叫方式，使用api

--- src/main/java/ntou/soselab/chatops4msa/Entity/ToolkitFunction/TrelloToolkit.java
+++ src/main/java/ntou/soselab/chatops4msa/Entity/ToolkitFunction/TrelloToolkit.java
@@ -1,24 +1,38 @@
 package ntou.soselab.chatops4msa.Entity.ToolkitFunction;
 
 import ntou.soselab.chatops4msa.Service.DiscordService.JDAService;
-import ntou.soselab.chatops4msa.Service.TrelloService.CreateService;
-import org.springframework.beans.factory.annotation.Autowired;
+import okhttp3.*;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
 public class TrelloToolkit extends ToolkitFunction {
-    private final CreateService createService;
 
-    @Autowired
-    public TrelloToolkit(CreateService createService) {
-        this.createService = createService;
+    public void toolkitTrelloCreateCard(String board_id, String list_name, String card_name, String card_desc) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
 
-    }
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body = RequestBody.create(mediaType, "boardId=" + board_id +
+                "&listName=" + list_name +
+                "&cardName=" + card_name +
+                "&cardDesc=" + card_desc);
 
-    public void toolkitTrelloCreateCard(String board_id, String list_name, String card_name, String card_desc) throws IOException, InterruptedException {
-        createService.CreateCard(board_id, list_name, card_name, card_desc);
+        Request request = new Request.Builder()
+                .url("http://localhost:9000/api/createCard")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                System.out.println("Request was successful");
+                System.out.println("Response body: " + response.body().string());
+            } else {
+                System.out.println("Request failed with code: " + response.code());
+            }
+        }
     }
 }
 
