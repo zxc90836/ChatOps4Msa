662d37336127ad46f94a7f36828757da9d302781
zxc90836 <0979710326a@gmail.com>
2025-08-03 17:14:01 +0800

    fix: sonar import

--- src/main/java/ntou/soselab/chatops4msa/Entity/ToolkitFunction/SonarToolkit.java
+++ src/main/java/ntou/soselab/chatops4msa/Entity/ToolkitFunction/SonarToolkit.java
@@ -1,10 +1,9 @@
 package ntou.soselab.chatops4msa.Entity.ToolkitFunction;
 
-import org.springframework.boot.configurationprocessor.json.JSONArray;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
-import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
